709831f5b01641e7fb216d51eb3f4fee30bea86c
Blevy2 <benjamin.levy@noaa.gov>
2022-06-07 10:27:29 -0400

    Codes to calculate percent area occupied in each strata during survey months and a script to conduct stratified mean calculations on individual simulation output and calulate/plot error related to this

---
+++ TestScripts/percent_area_occupied.R
@@ -0,0 +1,347 @@
+#idea: maybe stratified random mean calculations dont match well because populations
+#are in the outer/small strata so that samples there are not weighted as much
+
+#this script explores the idea by looking at the percent of the population in each strata
+#during sampling weeks and compares it to the percent of cells in each stratum (which 
+#represents the weighting of the given stratum)
+
+library(tibble)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(here)
+
+#to rotate matrix before fields::image.plot
+rotate <- function(x) t(apply(x, 2, rev))
+
+
+#used in loops and to determine how many species
+short_names <- c("YT","Cod","Had") 
+#strata that each species occupies. Used to remove unneccesary columsn fro survey for each
+strata_species <- list()
+strata_species[["YT"]] <-  c(13,14,15,16,17,18,19,20,21)
+strata_species[["Cod"]] <- c(13,14,15,16,17,18,19,20,21,22,23,24,25)
+strata_species[["Had"]] <- c(13,14,15,16,17,18,19,20,21,22,23,24,25,29,30)
+
+
+#number of extra years in simulation. subtract this value from year in log matrix
+years_cut <- 2
+
+#FIRST READ IN RES WHICH CONTAINS SPATIAL OUTPUT FOR GIVEN SCENARIO
+
+res <- readRDS("E:\\READ-PDB-blevy2-MFS2\\GB_Results\\ConPop_ConTemp\\res_ConPop_ConTemp.RDS")
+
+#read in habitat
+hab <- readRDS(file="hab_GB_3species.RDS") #courser resolution
+
+#use survey log matrix from given scenario to find all population values in each strata of given sample week
+
+
+species_logmat <- vector("list",length(short_names))
+names(species_logmat) <- short_names
+
+
+for(s in short_names){
+  #be sure to eliminate any strata a given species does not occupy
+ temp <- res$survey$log.mat[(res$survey$log.mat[,4] %in% strata_species[[s]]),] #stratum is in row 4
+ #only take one sample per week  in each year
+ species_logmat[[s]] <- temp[!duplicated(temp[,c("year","week")]),]
+   }
+
+
+#MAKE A TEMPLATE FOR EACH STRATUM. THIS WILL BE USED TO EXTRACT VALUES FROM EACH STRATUM
+strat_locs <- vector("list",length(short_names))
+names(strat_locs) <- short_names
+for(s in short_names){
+  for(strat in strata_species[[s]]){
+    
+    temp <- match(hab$stratas , strat)
+    strat_locs[[s]][[strat]] <- matrix(temp,nc = ncol(hab$stratas))
+
+      }
+  }
+
+#GO THEOUGH SPECIES_LOGMAT FOR EACH SPECIES AND POPULATE WITH SPECIES VALUES IN 
+#EACH STRATA AND A COLUMN FOR TOTAL POPULATION
+
+spatial_pop <- vector("list",length(short_names))
+names(spatial_pop) <- short_names
+
+
+
+for(s in short_names){ #go through each species
+  
+  temp <- matrix(nr =length(species_logmat[[s]][,1]), nc = (length(strata_species[[s]]) + 1) ) #temp container to store values before appending to log mat. extra cell for total pop
+  spatial_pop_temp <- vector("list",52)
+  
+
+  for(i in seq(length(species_logmat[[s]][,1]))){ #go through each sample week
+    
+
+    idx <- 1
+    
+    for(strat in strata_species[[s]]){ #go through each stratum
+    
+      wk = as.numeric(species_logmat[[s]][i,"week"])
+      yr = as.numeric(species_logmat[[s]][i,"year"]) - years_cut
+      x = as.numeric(species_logmat[[s]][i,"x"])
+      y = as.numeric(species_logmat[[s]][i,"y"])
+      str = as.numeric(species_logmat[[s]][i,"stratum"])
+      
+    temp[i,idx] <- sum(   res$pop_bios[[(yr-1)*52 + wk]][[s]]  * strat_locs[[s]][[strat]], na.rm=T ) #results in matrix where only nonzero population are in the given strata. just sum it
+     idx <- idx+1
+     
+     #pull out spatial population values in first week of each season
+     if(wk == 13 || wk == 37){
+       
+       spatial_pop_temp[[wk]][[yr]] <-  res$pop_bios[[(yr-1)*52 + wk]][[s]] / sum( res$pop_bios[[(yr-1)*52 + wk]][[s]], na.rm=T)
+       
+     }
+ 
+    }
+  
+    temp[i,idx] <- sum(temp[i,(1:(idx-1))]) #total pop
+    temp[i,1:(idx-1)] <-  temp[i,1:(idx-1)] / temp[i,idx] #convert to percentage
+    colnames(temp) <- c(strata_species[[s]],"Total Pop")
+    
+    spatial_pop[[s]] <- spatial_pop_temp 
+ 
+  }
+  View(temp)
+  #append temp to species_logmat
+  species_logmat[[s]] <- cbind(species_logmat[[s]],temp)
+  
+}
+
+
+
+#take strata columns and turn them into rows for plotting
+library(tidyr)
+species_pct <- list()
+for(s in short_names){
+  print(s)
+ species_pct[[s]]  <- as.data.frame(species_logmat[[s]]) %>%
+  pivot_longer(cols = seq((length(species_logmat[[s]][1,])-length(strata_species[[s]])),(length(species_logmat[[s]][1,]))-1), values_to = "% Pop") 
+}
+
+
+
+
+#calculate percent of total cells for each strata
+total_cells <- vector("list",length(short_names)) #total cells in each domain
+for(s in short_names){
+  cells <- vector()
+  idx<-1
+  for(c in strata_species[[s]]){
+    cells[idx] <- sum(strat_locs[[s]][[c]],na.rm=T)
+    idx<- idx+1
+  }
+  total_cells[[s]] <- sum(cells)
+  
+}
+strata_pct <- vector("list",length(short_names))
+for(s in short_names){
+  for(c in strata_species[[s]]){
+    strata_pct[[s]][[c]] <- sum(strat_locs[[s]][[c]],na.rm=T) / total_cells[[s]]
+  }
+  
+}
+
+
+
+
+#make table that has stratum in one column and percent of area occupied by each strata in other
+#will be one for each species
+#join this table with species_pct of same name using the column stratum
+species_pct2 <- list()
+
+
+for(s in short_names){
+  temp <- data.frame()
+  idx <- 1
+  for(strat in strata_species[[s]]){
+    
+    temp[idx,1] <- as.character(strat)
+    temp[idx,2] <- as.numeric(strata_pct[[s]][[strat]])
+    idx <- idx+1
+    
+    colnames(temp, do.NULL = FALSE)
+    colnames(temp) <- c("name","pct area") #label 'name' is to match the stratum column label in species_pct
+    
+      }
+   print(temp)
+  species_pct2[[s]] <- species_pct[[s]] %>%
+    left_join(temp, by="name")
+  View(species_pct[[s]])
+}
+
+
+
+
+
+
+
+#################################################################################
+# PLOTS
+#################################################################################
+
+#PLOT SPATIAL POPULATION IN FIRST WEEK OF EACH SURVEY SEASON 2 WAYS
+#1) 1X2 PLOT OF EACH FIRST WEEK (USE FOR CONSTANT TEMP SITUATION)
+#AND
+#2) 5X4 PLOT OF EACH FIRST WEEK (1 FOR EACH YEAR OVER ENTIRE SIMULATION). USE FOR INCREASING TEMP SITUATION
+
+plot_wks <- c(13,37) #first week of each survey season
+
+
+
+
+
+
+
+#FIRST, 1) 1X2 PLOT OF EACH FIRST WEEK (USE FOR CONSTANT TEMP SITUATION)
+
+#creating new breaks for plotting
+Qbreaks_list <- list()
+
+
+#first for population values
+for(s in short_names){
+  Qbreaks2 <- list()
+  for(wk in plot_wks){
+                                          #just using first image to set breaks since thats what we will plot
+    Qbreaks <- classInt::classIntervals(var=as.vector(spatial_pop[[s]][[wk]][[1]]), style = "fisher") 
+    
+    # #remove zeros from breaks
+    # Qbreaks2[[wk]] <- Qbreaks[!Qbreaks[["brks"]] %in% 0]
+    # Qbreaks2[[wk]] <- append(0,Qbreaks2)#put single 0 back to start
+    # 
+    Qbreaks_list[[s]][[wk]] <- Qbreaks
+  }
+}
+
+
+for(s in short_names){
+  par( mfrow = c(1,2), mar = c(1, 1, 1, 1))
+  for(wk in plot_wks){
+  
+    #WITHOUT USING BREAKS
+   # fields::image.plot(rotate(spatial_pop[[s]][[wk]][[1]]))
+    
+    #WITH USING BREAKS
+    fields::image.plot(rotate(spatial_pop[[s]][[wk]][[1]]), breaks = Qbreaks_list[[s]][[wk]]$brks, nlevel = (length( Qbreaks_list[[s]][[wk]]$brks)-1) )
+    text(0.5, 0.98, labels = paste( s, 'Week', wk), cex = 1)
+    
+  }
+}
+
+
+
+#SECOND, 2) 5X4 PLOT OF EACH FIRST WEEK (1 FOR EACH YEAR OVER ENTIRE SIMULATION). USE FOR INCREASING TEMP SITUATION
+
+#creating new breaks for plotting
+Qbreaks_list <- list()
+
+
+#first for population values
+for(s in short_names){
+  Qbreaks2 <- list()
+  for(wk in plot_wks){
+    
+    Qbreaks <- classInt::classIntervals(var=unlist(as.vector(spatial_pop[[s]][[wk]])), style = "fisher") 
+    
+    # #remove zeros from breaks
+    # Qbreaks2[[wk]] <- Qbreaks[!Qbreaks[["brks"]] %in% 0]
+    # Qbreaks2[[wk]] <- append(0,Qbreaks2)#put single 0 back to start
+    # 
+    Qbreaks_list[[s]][[wk]] <- Qbreaks
+  }
+}
+
+
+
+pdf(file=paste0('testfolder/Spatial_pop_plots','.pdf'))
+
+for(s in short_names){
+
+  for(wk in plot_wks){ 
+    par( mfrow = c(5,4), mar = c(1,1,1,1))
+    for(yr in seq(length(spatial_pop[[s]][[wk]]))){
+    
+    #WITHOUT USING BREAKS
+   #fields::image.plot(rotate(spatial_pop[[s]][[wk]][[yr]]))
+      
+    #WITH USING BREAKS
+    fields::image.plot(rotate(spatial_pop[[s]][[wk]][[yr]]), breaks = Qbreaks_list[[s]][[wk]]$brks, nlevel = (length( Qbreaks_list[[s]][[wk]]$brks)-1))
+    text(0.5, 0.98, labels = paste( s, 'Week', wk, "Year", yr), cex = 1)
+    
+    }
+        par( mfrow = c(1,2), mar = c(1,1,1,1))
+    #plot first and last years for easy comparison
+    fields::image.plot(rotate(spatial_pop[[s]][[wk]][[1]]), breaks = Qbreaks_list[[s]][[wk]]$brks, nlevel = (length( Qbreaks_list[[s]][[wk]]$brks)-1) )
+    text(0.5, 0.98, labels = paste( s, 'Week', wk, "Year", 1), cex = 1)
+    
+    fields::image.plot(rotate(spatial_pop[[s]][[wk]][[20]]), breaks = Qbreaks_list[[s]][[wk]]$brks, nlevel = (length( Qbreaks_list[[s]][[wk]]$brks)-1) )
+    text(0.5, 0.98, labels = paste( s, 'Week', wk, "Year", 20), cex = 1)
+    
+
+    
+    
+  }
+}
+dev.off()
+
+
+
+#PLOT PERCENT OF POPULATION IN EACH STRATA IN EACH WEEK OF SURVEY 
+#WITH HORIZONTAL LINE REPRESENTING PERCENT AREA COVERED BY GIVEN STRATA
+
+library(ggplot2)
+long_names <- c("Yellowtail Flounder","Cod","Haddock")
+idx<-1
+for(s in short_names){
+p<- ggplot() +
+  geom_line(data=species_pct2[[s]], aes(x=as.numeric(year), y=as.numeric(`% Pop`), group=week, color=week))+
+  geom_point(data=species_pct2[[s]], aes(x=as.numeric(year), y=as.numeric(`% Pop`), group=week, color=week))+
+  
+  geom_hline(data = species_pct2[[s]],aes( yintercept = as.numeric(`pct area`), linetype = "")) +
+  scale_linetype_manual(name = "Stratum %", values = c(2, 2)) +
+                        
+  facet_wrap(~ name)+
+labs(x="Year",y="% Pop in Each Stratum", title = long_names[idx], color ="" )
+idx<-idx+1
+print(p)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TestScripts/stratified_mean_limited.R
@@ -0,0 +1,381 @@
+#THIS SCRIPT IS A PIECE OF RUN_SURVEY_BENS_NEW WHERE THE STRATIFIED MEAN CALCULATION IS MADE
+#THIS SCRIPT ALLOWS US TO CALULATE THE STRATIFIED MEAN FOR A SMALLER NUMER OF NOISELESS SURVEYS
+
+
+
+##################################################################################################
+#THINGS WE NEED
+##################################################################################################
+scenario <- "DecPop_ConTemp"
+
+n_spp <- 3
+
+#survey results without noise
+list_all <- readRDS(paste("list_all_",scenario,".RDS",sep=""))
+
+#simulation results
+result <- readRDS(paste("E:\\READ-PDB-blevy2-MFS2\\GB_Results\\",scenario,"\\result_",scenario,".RDS",sep=""))
+
+#random survey locations
+surv_random <- readRDS(paste("E:\\READ-PDB-blevy2-MFS2\\GB_Results\\",scenario,"\\surv_random_",scenario,".RDS",sep=""))
+
+#spp1 spp2 spp3
+short_names <- c("YT","Cod","Had") 
+
+#strata that each species occupies. Used to calculate stratified random mean of each
+strata_species <- list()
+strata_species[["YT"]] <-  c(13,14,15,16,17,18,19,20,21)
+strata_species[["Cod"]] <- c(13,14,15,16,17,18,19,20,21,22,23,24,25)
+strata_species[["Had"]] <- c(13,14,15,16,17,18,19,20,21,22,23,24,25,29,30)
+
+##################################################################################################
+
+
+
+
+#BELOW WILL TAKE A MINUTE
+
+#choose some strata to exclude, if desired
+
+
+#George's Bank Setup by species
+#YT            Cod          Haddock
+exclude <- list(c(13,14,15,17,18), c(23,24,25), c(23,24,25,29,30))
+
+#exclude none
+exclude <- list(c(0),c(0),c(0)) #3 species
+
+
+#NOW WE NEED TO CREATE A STRATIFIED MEAN FROM EACH OF THESE SAMPLES
+#there are #strata * #iterations * #samp_per_iter total samples
+
+#I AM COPYING FROM CALC_SRS_INDEX_SURVEY_BENS, which was adapted from Liz's code to create below
+library(tibble)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(here)
+
+#stop output from below using this option
+options(dplyr.summarise.inform = FALSE)
+
+#load file to calculate the stratified mean
+source("TestScripts/Calc_strat_mean/fn_srs_survey_BENS.R")
+
+
+
+#setup dimensions for each species- 1 for each strata
+strat_mean_all <- vector("list",length(seq(n_spp)))
+
+for(s in seq(n_spp)){
+  
+  strat_mean_all[[s]] <- vector("list",length(surv_noise)) 
+}
+
+
+
+#go through each strata survey, iteration, sample
+
+
+for(iter in seq(length(list_all))){
+  print(iter)
+
+    #if any NA columns make them zero
+    list_all[[iter]][is.na(list_all[[iter]])]=0
+    
+    
+    
+    # calculate SRS estimates ====
+    
+    for(s in seq(n_spp)){
+      
+      
+      #DEFINE INDIVIDUAL STRATUM AREAS 
+      stratum <- sort(unique(surv_random$log.mat[,4]))
+      
+      STRATUM_AREA <- na.omit(surv_random$cells_per_strata) # old way: rep(10000/nstrata,nstrata) #100x100 grid so each corner has area 2500
+      
+      sv.area <- as_tibble(data.frame(stratum,STRATUM_AREA))
+      
+      #remove stratum that species does not occupy
+      sv.area <- sv.area[(sv.area$stratum %in% strata_species[[short_names[s]]]),]#sv.area %>% slice(-exclude)
+      
+      #remove strata to exclude from stratified mean calculation
+      sv.area <- sv.area[!(sv.area$stratum %in% exclude[[s]]),]#sv.area %>% slice(-exclude)
+      
+      spp <- as_tibble(list_all[[iter]],header=T) #pull out entire survey matrix
+      
+      ##remove strata to exclude from stratified mean calculation
+      spp <- spp[(spp$stratum %in% strata_species[[short_names[s]]]),]
+      spp <- spp[!(spp$stratum %in% exclude[[s]]),]
+      
+      spp$year <- as.numeric(spp$year)
+      
+      
+      # get total area of stock ====
+      spp.strata <- unique(spp$stratum)
+      spp.strata <- as.numeric(spp.strata)
+      
+      spp.area <- sum(sv.area$STRATUM_AREA[sv.area$stratum %in% spp.strata]) #TOTAL AREA OF ALL STRATA
+      
+      
+      
+      temp <- srs_survey(df=spp, sa=sv.area, str=NULL, ta=1, sppname = paste0("spp", s, sep="")  )   # if strata=NULL, the function will use the unique strata set found in df
+      # View(temp)
+      strat_mean_all[[s]][[iter]] <- temp %>%
+        mutate(mean.yr.absolute=mean.yr*spp.area, sd.mean.yr.absolute=sd.mean.yr*spp.area,
+               CV.absolute=sd.mean.yr.absolute/mean.yr.absolute) # if strata=NULL, the function will use the unique strata set found in df
+      
+      strat_mean_all[[s]][[iter]] <- data.matrix(strat_mean_all[[s]][[iter]])
+      
+     
+      
+    }
+  
+    colnames(strat_mean_all[[s]][[iter]]) <- c("year","mean.yr","var.mean.yr","sd.mean.yr","CV","season","mean.yr.absolute","sd.mean.yr.absolute","CV.absolute")
+    
+  
+  
+  
+}
+
+
+names(strat_mean_all) <- short_names
+
+
+
+##########################################################################################
+# Preparing things to plot
+##########################################################################################
+
+
+#copying plot_pop_summary to summarize yearly population estimates
+#assumes we have summarized version of simulations called res
+results_df <- list()
+
+for(iter in seq(length(list_all))){
+  
+res <- result[[iter]]
+
+n_spp <- length(res[["pop_summary"]]) 
+res_df <- lapply(seq_len(n_spp), function(x) {
+  res_spp <- lapply(names(res[["pop_summary"]][[x]]), function(x1) {
+    x1_res <- tidyr::gather(as.data.frame(t(res[["pop_summary"]][[x]][[x1]])), key = "year", factor_key = T)
+    if(x1 == "Bio.mat" | x1 == "Bio.mat.sd") {	res_out <- data.frame("pop" = rep(short_names[[x]], length.out = nrow(x1_res)), 
+                                                                     "metric" = x1, 
+                                                                     "year" = as.numeric(x1_res$year), 
+                                                                     "day" = rep(1:358, length.out = nrow(x1_res)),#changed 362 to 358
+                                                                     "julien_day" = seq_len(nrow(x1_res)),
+                                                                     "data" = x1_res$value) 
+    
+    return(res_out)
+    
+    }
+    if(x1 == "Rec.mat" | x1 == "Rec.mat.sd") { res_out <- data.frame("pop" = rep(short_names[[x]], length.out = nrow(x1_res)), 
+                                                                     "metric" = x1 , 
+                                                                     "year" = as.numeric(seq_len(nrow(x1_res))), 
+                                                                     "day" = rep(1, length.out = nrow(x1_res)),
+                                                                     "julien_day" = rep(1, length.out = nrow(x1_res)),
+                                                                     "data" = x1_res$value) 
+    
+    return(res_out)
+    
+    }
+    
+  })
+  return(do.call(rbind, res_spp))
+})
+
+results_df[[iter]] <- do.call(rbind, res_df)
+
+}
+
+
+
+
+
+# #ANNUAL POP BY SPECIES
+
+annual_species <- list()
+
+for(iter in seq(length(list_all))){
+
+for(s in seq(length(short_names))){
+  annual_species[[short_names[s]]][[iter]] <- results_df[[iter]] %>% filter(metric == "Bio.mat", day == 1, pop == short_names[s]) %>% 
+    group_by(pop,year) %>% summarise(data = sum(data))
+  
+}
+}
+
+
+
+
+pdf(file=paste("Results/GB_error_plots/Individual_SRS_",scenario,".pdf",sep=""))
+
+
+nyears <- 20
+
+#for error calculation
+SRS_error_spring <- list()
+SRS_error_fall <- list()
+model <- list()
+SRS_spring <- list()
+SRS_fall <- list()
+
+#plot stratified calculation and population estimate on same plot
+
+#first make model output have 2 seasons to match the stratified mean calcs
+
+for(iter in seq(length(list_all))){
+  
+  print(iter)
+  
+for(s in seq(length(annual_species))){
+  annual_species[[short_names[s]]][[iter]] <- rbind(annual_species[[short_names[s]]][[iter]][3:22,],annual_species[[short_names[s]]][[iter]][3:22,])
+  annual_species[[short_names[s]]][[iter]]$season <- c(rep(1,nyears),rep(2,nyears)) #spring = season 1, fall = season 2
+  annual_species[[short_names[s]]][[iter]]$year <- as.numeric(rep(seq(3,22),2))
+}
+  
+
+
+SRS_data <- list()
+
+
+for(s in short_names){ 
+ 
+  #pull out strat mean calc
+  SRS_data[[s]] <-  strat_mean_all[[s]][[iter]] 
+  
+  #model value 
+  model[[s]][[iter]] <-  annual_species[[s]][[iter]][annual_species[[s]][[iter]]$season==1,"data"]
+  
+  #SRS spring estimate
+  SRS_spring[[s]][[iter]] <- strat_mean_all[[s]][[iter]][strat_mean_all[[s]][[iter]][,"season"]==1,"mean.yr.absolute"]
+  
+  #SRS fall estimate
+  SRS_fall[[s]][[iter]] <- strat_mean_all[[s]][[iter]][strat_mean_all[[s]][[iter]][,"season"]==2,"mean.yr.absolute"]
+  
+  #calculate SPRING error from each iteration
+  SRS_error_spring[[s]][[iter]] <- norm(model[[s]][[iter]] - SRS_spring[[s]][[iter]] , type="2") / norm(model[[s]][[iter]] , type ="2")
+  
+  #calculate FALL error from each iteration
+  SRS_error_fall[[s]][[iter]] <- norm(model[[s]][[iter]] - SRS_fall[[s]][[iter]] , type="2") / norm(model[[s]][[iter]] , type ="2")
+  
+    }
+
+
+long_names <- c("Yellowtail Flounder","Cod","Haddock")
+
+idx <-1
+par(mfrow = c(1,3), mar = c(1, 1, 1, 1))
+
+#for(s in short_names){
+  # 
+  # #OLD WAY
+  # #initiate ggplot
+  # p<- ggplot() +
+  #   #plot stratified calculation data
+  #   geom_errorbar(data=as.data.frame(SRS_data[[s]]),aes(x=year,y=mean.yr.absolute,group=season,ymin=mean.yr.absolute-(1.96*sd.mean.yr.absolute), ymax=mean.yr.absolute+(1.96*sd.mean.yr.absolute), color = "Stratified Mean"),width=.3) +
+  #   geom_point(data=as.data.frame(SRS_data[[s]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+  #   geom_line(data=as.data.frame(SRS_data[[s]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+  #   #plot model data
+  #   geom_point(data = as.data.frame(annual_species[[s]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+  #   geom_line(data = as.data.frame(annual_species[[s]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+  #   
+  #   facet_wrap(~ season) +
+  #   labs(x="year",y="Biomass", title = long_names[idx], color ="" ) 
+  # idx<-idx+1
+  # 
+  # print(p)
+  #}
+  
+  #NEW WAY PLOTTING 3 TOGETHER ON SAME PAGE
+    
+    #YTF
+    p1<- ggplot() +
+    #plot stratified calculation data
+    geom_errorbar(data=as.data.frame(SRS_data[[1]]),aes(x=year,y=mean.yr.absolute,group=season,ymin=mean.yr.absolute-(1.96*sd.mean.yr.absolute), ymax=mean.yr.absolute+(1.96*sd.mean.yr.absolute), color = "Stratified Mean"),width=.3) +
+    geom_point(data=as.data.frame(SRS_data[[1]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+    geom_line(data=as.data.frame(SRS_data[[1]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+    #plot model data
+    geom_point(data = as.data.frame(annual_species[[1]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+    geom_line(data = as.data.frame(annual_species[[1]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+    
+    facet_wrap(~ season) +
+    labs(x="year",y="Biomass", title = long_names[1], color ="" ) 
+
+    #COD
+    p2<- ggplot() +
+      #plot stratified calculation data
+      geom_errorbar(data=as.data.frame(SRS_data[[2]]),aes(x=year,y=mean.yr.absolute,group=season,ymin=mean.yr.absolute-(1.96*sd.mean.yr.absolute), ymax=mean.yr.absolute+(1.96*sd.mean.yr.absolute), color = "Stratified Mean"),width=.3) +
+      geom_point(data=as.data.frame(SRS_data[[2]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+      geom_line(data=as.data.frame(SRS_data[[2]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+      #plot model data
+      geom_point(data = as.data.frame(annual_species[[2]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+      geom_line(data = as.data.frame(annual_species[[2]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+      
+      facet_wrap(~ season) +
+      labs(x="year",y="Biomass", title = long_names[2], color ="" )
+    
+    #HAD
+    p3<- ggplot() +
+      #plot stratified calculation data
+      geom_errorbar(data=as.data.frame(SRS_data[[3]]),aes(x=year,y=mean.yr.absolute,group=season,ymin=mean.yr.absolute-(1.96*sd.mean.yr.absolute), ymax=mean.yr.absolute+(1.96*sd.mean.yr.absolute), color = "Stratified Mean"),width=.3) +
+      geom_point(data=as.data.frame(SRS_data[[3]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+      geom_line(data=as.data.frame(SRS_data[[3]]),aes(x=year,y=mean.yr.absolute,group=season, color = "Stratified Mean"))+
+      #plot model data
+      geom_point(data = as.data.frame(annual_species[[3]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+      geom_line(data = as.data.frame(annual_species[[3]][[iter]]), aes(x=as.numeric(year),y=data, group =season, color = "Model")) +
+      
+      facet_wrap(~ season) +
+      labs(x="year",y="Biomass", title = long_names[3], color ="" )
+
+    gridExtra::grid.arrange(p1,p2,p3,nrow=3)
+
+
+}
+
+dev.off()
+
+
+
+
+
+
+df_spring <- tibble(iter = rep(1:length(list_all),n_spp),
+                  error = c(SRS_error_spring[[1]], SRS_error_spring[[2]], SRS_error_spring[[3]]),
+                  species = c(rep("YTF",length(list_all)),rep("Cod",length(list_all)),rep("Had",length(list_all))),
+                  
+                  season = rep(rep("spring",length(list_all)),n_spp),
+)
+
+df_fall <- tibble(iter = rep(1:length(list_all),n_spp),
+             error = c(SRS_error_fall[[1]], SRS_error_fall[[2]], SRS_error_fall[[3]]),
+             species = c(rep("YTF",length(list_all)),rep("Cod",length(list_all)),rep("Had",length(list_all))),
+             
+             season = rep(rep("fall",length(list_all)),n_spp),
+             )
+
+df <- rbind(df_fall,df_spring)
+
+#Error scatterplots
+
+
+library(ggplot2)
+ggplot(data=df,
+       aes(x=iter,y=as.numeric(error))) +
+  geom_point()+
+  facet_grid(season ~ species)
+
+# 
+# ggsave(filename = paste("Results/GB_error_plots/Individussssal_SRS_",scenario,".pdf",sep=""),
+#        plot = last_plot())
+# 
+
+
+
+
+
+
