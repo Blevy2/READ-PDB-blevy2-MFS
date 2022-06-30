05ac594af04aa0cdf5bd9b911f26eb20d4ac8109
Blevy2 <benjamin.levy@noaa.gov>
2022-06-30 10:54:59 -0400

    model selection with YT

--- VAST/Test_3Species_GeorgesBank_YT_Only.R
+++ VAST/Test_3Species_GeorgesBank_YT_Only.R
@@ -18,7 +18,7 @@ surv_random_sample <- as.matrix(surv_random_sample,ncol= 12)
 scenario <- "ConPop_IncTemp"
 #survey results without noise
 list_all <- readRDS(paste("E:\\READ-PDB-blevy2-MFS2\\GB_Results\\",scenario,"\\list_all_more_",scenario,".RDS",sep=""))
-surv_random_sample <- list_all[[56]]
+surv_random_sample <- list_all[[1]]
 ################################################################################
 
 #read in habitat matrix
@@ -71,21 +71,6 @@ surv_random_sample <- cbind(surv_random_sample,lat,lon)
 colnames(surv_random_sample) <- c("station_no","x","y","stratum","day","tow","year","YTF","Cod","Had","week","Season","Lat","Lon")
 
 
-#YT         
-exclude <- c(13,14,15,17,18)
-
-#exclude none
-exclude <- c(0)
-
-strata_species <-  c(13,14,15,16,17,18,19,20,21)
-
-#following from Chris' file...
-
-adios <- as.data.frame(surv_random_sample)
-adios <- adios[(adios$stratum %in% strata_species),]
-adios <- adios[!(adios$stratum %in% exclude),]
-
-head(adios)
 
 #FIGURE OUT HOW BIG EACH CELL OF RASTER IS IN KM^2 TO SET AREASWEPT_KM2 SETTING BELOW
 
@@ -131,6 +116,42 @@ cell_str <- surv_random$cells_per_strata[!is.na(surv_random$cells_per_strata)]
 area_per_cell <- st_area/cell_str
 
 
+#change directory
+setwd(paste(getwd(),"/VAST", sep=""))
+#create new one
+dir.create(paste(getwd(),"/",scenario,sep=""))
+
+
+#YT         
+exclude <- c(13,14,15,17,18)
+
+#exclude none
+exclude <- c(0)
+
+strata_species <-  c(13,14,15,16,17,18,19,20,21)
+
+#do some model selection things
+model_aic <- list()
+
+for(j in 1){
+  
+  if(j == 1) {obsmodel <- c(2, 0); run <- 1}
+  if(j == 2) {obsmodel <- c(2, 1); run <- 3} #model selection
+  if(j == 3) {obsmodel <- c(1, 0); run <- 4}
+  if(j == 4) {obsmodel <- c(1, 1); run <- 5}
+  
+  #create directory for model specific output
+  dir.create(paste(getwd(),"/",scenario,"/obsmodel",j,sep=""))
+  setwd((paste(getwd(),"/",scenario,"/obsmodel",j,sep="")))
+  
+  #following from Chris' file...
+  
+  adios <- as.data.frame(surv_random_sample)
+  adios <- adios[(adios$stratum %in% strata_species),]
+  adios <- adios[!(adios$stratum %in% exclude),]
+  
+  head(adios)
+
 #PULLING OUT YELLOTWTAIL FLOUNDER IN THIS SCRIPT
 
 # format for use in VAST
@@ -144,8 +165,8 @@ spring <- adios %>%
          Lon = Lon) %>%
   mutate(Vessel = "missing",
          AreaSwept_km2 = mean(cell_size)) #CORRECT AREA SWEPT?
-summary(spring)
-names(spring)
+# summary(spring)
+# names(spring)
 
 # reorder the data for use in VAST
 #DOESNT SEEM TO BE USED BELOW...??
@@ -164,11 +185,12 @@ example$strata.limits <- data.frame(Georges_Bank = c(1130, 1140, 1150, 1160, 117
 
 #make_settings seems like the way to impliment most desired settings
  
-# settings <- make_settings(n_x = 50,
-#                           Region=example$Region, 
-#                           purpose="index2", 
-#                           strata.limits=example$strata.limits, 
-#                           bias.correct=TRUE)
+# settings <- make_settings(n_x = 500,
+#                           Region=example$Region,
+#                           purpose="index2",
+#                           strata.limits=example$strata.limits,
+#                           bias.correct=TRUE,
+#                           ObsModel = obsmodel)
 #ABOVE SETTINGS PRODUCE ERRORS. CHECK_FIT SUGGESTS ADDITIONAL FIELDCONFIG SETTINGS
 
 #WHEN ADDING ADDITIONAL FIELDCONFIG SETTINGS ALL 4 SETTINGS BELOW MUST BE INCLUDED
@@ -177,16 +199,20 @@ settings <- make_settings(n_x = 1000,  #NEED ENOUGH KNOTS OR WILL HAVE ISSUES WI
                           purpose="index2",
                           strata.limits=example$strata.limits,
                           bias.correct=TRUE,
-                          FieldConfig= c("Omega1"=0, "Epsilon1"=0, "Omega2"=0, "Epsilon2"=0))
+                          FieldConfig= c("Omega1"=1, "Epsilon1"=0, "Omega2"=1, "Epsilon2"=0),
+                          ObsModel = obsmodel)
 #' Specification of \code{FieldConfig} can be seen by calling \code{\link[FishStatsUtils]{make_settings}},
 #'   which is the recommended way of generating this input for beginning users.
 #dafault FieldConfig settings:
 # if(missing(FieldConfig)) FieldConfig = c("Omega1"=0, "Epsilon1"=n_categories, "Omega2"=0, "Epsilon2"=0)
 
-settings
+#settings
 
-setwd("C:\\Users\\benjamin.levy\\Desktop\\Github\\READ-PDB-blevy2-MFS2\\VAST\\ConPop_IncTemp_exclude_most")
+#setwd("C:\\Users\\benjamin.levy\\Desktop\\Github\\READ-PDB-blevy2-MFS2\\VAST\\ConPop_IncTemp_exclude_most")
 
+#create directory for season specific output
+dir.create(paste(getwd(),"/spring",sep=""))
+setwd(paste(getwd(),"/spring",sep=""))
 
 #######################################################################################
 # Try this first
@@ -206,6 +232,10 @@ fit_spring <- fit_model(settings = settings,
 plot_biomass_index(fit_spring)
 
 
+setwd('..') #move up one directory
+dir.create(paste(getwd(),"/fall",sep="")) #create fall directory
+setwd(paste(getwd(),"/fall",sep=""))  #set it
+
 
 fall <- adios %>%
   filter(Season == "FALL") %>%
@@ -217,8 +247,8 @@ fall <- adios %>%
          Lon = Lon) %>%
   mutate(Vessel = "missing",
          AreaSwept_km2 = mean(cell_size)) #CORRECT AREA SWEPT?
-summary(fall)
-names(fall)
+# summary(fall)
+# names(fall)
 
 # reorder the data for use in VAST
 #DOESNT SEEM TO BE USED BELOW...??
@@ -237,21 +267,22 @@ example$strata.limits <- data.frame(Georges_Bank = c(1130, 1140, 1150, 1160, 117
 
 #make_settings seems like the way to impliment most desired settings
 
-# settings <- make_settings(n_x = 50,
-#                           Region=example$Region, 
-#                           purpose="index2", 
-#                           strata.limits=example$strata.limits, 
-#                           bias.correct=TRUE)
-#ABOVE SETTINGS PRODUCE ERRORS. CHECK_FIT SUGGESTS ADDITIONAL FIELDCONFIG SETTINGS
-
-#WHEN ADDING ADDITIONAL FIELDCONFIG SETTINGS ALL 4 SETTINGS BELOW MUST BE INCLUDED
-settings <- make_settings(n_x = 1000,  #NEED ENOUGH KNOTS OR WILL HAVE ISSUES WITH PARAMETER FITTING
+settings <- make_settings(n_x = 50,
                           Region=example$Region,
                           purpose="index2",
                           strata.limits=example$strata.limits,
                           bias.correct=TRUE,
-                          FieldConfig= c("Omega1"=0, "Epsilon1"=0, "Omega2"=0, "Epsilon2"=0),
-                          RhoConfig = c("Beta1" = 0, "Beta2" = 3, "Epsilon1" = 0, "Epsilon2" = 0))
+                          ObsModel = obsmodel)
+#ABOVE SETTINGS PRODUCE ERRORS. CHECK_FIT SUGGESTS ADDITIONAL FIELDCONFIG SETTINGS
+
+#WHEN ADDING ADDITIONAL FIELDCONFIG SETTINGS ALL 4 SETTINGS BELOW MUST BE INCLUDED
+# settings <- make_settings(n_x = 500,  #NEED ENOUGH KNOTS OR WILL HAVE ISSUES WITH PARAMETER FITTING
+#                           Region=example$Region,
+#                           purpose="index2",
+#                           strata.limits=example$strata.limits,
+#                           bias.correct=TRUE,
+#                           FieldConfig= c("Omega1"=0, "Epsilon1"=0, "Omega2"=0, "Epsilon2"=0),
+#                           RhoConfig = c("Beta1" = 0, "Beta2" = 3, "Epsilon1" = 0, "Epsilon2" = 0))
 #' Specification of \code{FieldConfig} can be seen by calling \code{\link[FishStatsUtils]{make_settings}},
 #'   which is the recommended way of generating this input for beginning users.
 #dafault FieldConfig settings:
@@ -280,6 +311,11 @@ fit_fall <- fit_model(settings = settings,
 plot_biomass_index(fit_fall)
 
 
+#go back to scenario directory before moving to next model case
+setwd('..')
+setwd('..')
+
+}
 
 
 
@@ -324,8 +360,5 @@ plot(fit)
 plot_biomass_index(fit, PlotName = "index2") #DirName = paste(directory) PlotName = paste(name) 
 
 
-
-
-#compare 
 
 
